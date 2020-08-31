455a81296e49331d44fc4b5790b965a451d24197
spencerkinney <contact@spencer.biz>
2020-08-31 00:56:08 -0500

    Cleaned up some of the code in views.py

--- sports_scraper/sports_scraper/views.py
+++ sports_scraper/sports_scraper/views.py
@@ -37,18 +37,13 @@ def get_pitchers_information(url):
     reqs = requests.get(url)
     soup = BeautifulSoup(reqs.text, 'lxml')
 
-    class_var = "col-group-start-sa9unvY0 number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT"
     players_parser = soup.find_all(class_="short-3OJ0bTju")
     positions_parser = soup.find_all(class_="position-28TbwVOg")
     teams_parser = soup.find_all(class_="col-group-end-2UJpJVwW number-aY5arzrB align-left-3L2SU-Mk is-table-pinned-1WfPW2jT")
     wins_parser = soup.find_all(attrs={"data-col" : "2"}, class_="col-group-start-sa9unvY0 number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
     lose_parser = soup.find_all(attrs={"data-col" : "3"}, class_="number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
     ERA_parser = soup.find_all(attrs={"data-col" : "4"}, class_="selected-1vxxHvFg col-group-end-2UJpJVwW number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
-    #atbats_parser = soup.find_all(attrs={"data-col" : "3"}, class_="col-group-end-2UJpJVwW number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
-    #runs_parser = soup.find_all(attrs={"data-col" : "4"}, class_="col-group-start-sa9unvY0 number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
-    #hits_parser = soup.find_all(attrs={"data-col" : "5"}, class_="number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
-
-    # Creates a list of attributes for each player ('players_sub_list') for each player and stores it in the 'player_list' list
+    
     players_list = []
     players_sublist = []
     playerCount = len(players_parser)
@@ -62,45 +57,8 @@ def get_pitchers_information(url):
         players_sublist.append(str(wins_parser[i].text))
         players_sublist.append(str(lose_parser[i].text))
         players_sublist.append(str(ERA_parser[i].text))
-        #players_sublist.append(str(atbats_parser[i].text))
-        #players_sublist.append(str(runs_parser[i].text))
-        #players_sublist.append(str(hits_parser[i].text))
         players_list.append(players_sublist)
-    return players_list
 
-def hitters(request):
-    hitters_url = 'https://www.mlb.com/stats/hitting'
-    
-    hitters_list = get_hitters_information(hitters_url)
-    '''
-    reqs = requests.get(players_url)
-    soup = BeautifulSoup(reqs.text, 'lxml')
-
-    players_parser = soup.find_all(class_="short-3OJ0bTju")
-    positions_parser = soup.find_all(class_="position-28TbwVOg")
-    teams_parser = soup.find_all(class_="col-group-end-2UJpJVwW number-aY5arzrB align-left-3L2SU-Mk is-table-pinned-1WfPW2jT")
-    gamesplayed_parser = soup.find_all(attrs={"data-col" : "2"}, class_="col-group-start-sa9unvY0 number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
-    atbats_parser = soup.find_all(attrs={"data-col" : "3"}, class_="col-group-end-2UJpJVwW number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
-    runs_parser = soup.find_all(attrs={"data-col" : "4"}, class_="col-group-start-sa9unvY0 number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
-    hits_parser = soup.find_all(attrs={"data-col" : "5"}, class_="number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT")
-
-    # Creates a list of attributes for each player ('players_sub_list') for each player and stores it in the 'player_list' list
-    players_list = []
-    players_sublist = []
-    playerCount = len(players_parser)
-
-    for i in range(playerCount):
-        players_sublist = []
-        players_sublist.append(i +1)
-        players_sublist.append(str(players_parser[i].text))
-        players_sublist.append(str(positions_parser[i].text))
-        players_sublist.append(str(teams_parser[i].text))
-        players_sublist.append(str(gamesplayed_parser[i].text))
-        players_sublist.append(str(atbats_parser[i].text))
-        players_sublist.append(str(runs_parser[i].text))
-        players_sublist.append(str(hits_parser[i].text))
-        players_list.append(players_sublist)
-    '''
     '''
 
     INPUT:   print(players_list)
@@ -109,6 +67,11 @@ def hitters(request):
 
     '''
 
+    return players_list
+
+def hitters(request):
+    hitters_url = 'https://www.mlb.com/stats/hitting'
+    hitters_list = get_hitters_information(hitters_url)
     return render(request, 'index.html', {'hitters_list': hitters_list})
 
 def pitchers(request):
